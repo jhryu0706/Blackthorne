19ad1dbbd92e32df9d2ff77eafd638b2c96da760
Irene J. Ryu <jatmandu@Irenes-MacBook-Air.local>
2023-04-19 11:33:06 -0500

    updates

--- Phase2.py
+++ Phase2.py
@@ -1,7 +1,9 @@
 from Phase1 import factor1, factor2, factor3, dict, _factorHnL
 import numpy as np
 import pandas as pd
-
+#make factorHnL an external variable
+f = _factorHnL()
+fb = _factorHnL('B')
 f1 = factor1()
 f2 =factor2()
 f3 = factor3()
@@ -62,7 +64,6 @@ def factorA_0304():
     high and low terms so use that
     define f which is output of _factorHnL() differently depending on whether A or B
     """
-    f = _factorHnL()
     dict2 ={}
     for x in dict.keys():
         dict2[x]=[]
@@ -76,20 +77,104 @@ def factorA_0304():
     return pd.DataFrame(dict2)
 
 def factorB_0304():
-    f = _factorHnL(Rule='B')
     dict2 ={}
     for x in dict.keys():
         dict2[x]=[]
     for comm in dict2.keys():
-        for i in range(len(f[1]['H'])):
-            if comm in f[1]['H'][i]:
+        for i in range(len(fb[1]['H'])):
+            if comm in fb[1]['H'][i]:
                 dict2[comm].append(1)
-            elif comm in f[1]['L'][i]:
+            elif comm in fb[1]['L'][i]:
                 dict2[comm].append(-1)
             else:
                 dict2[comm].append(0)
     return pd.DataFrame(dict2)
 
-
-
+def create_rocp1_dataframe():
+    dict_rocp1 = {}
+    for comm in dict.keys():
+        dict_rocp1[comm] = dict[comm]['rocp 1 day']
     
+    rocp1_df = pd.DataFrame(dict_rocp1)
+    return rocp1_df
+    
+
+rocp1_dataframe = create_rocp1_dataframe()
+
+
+def calculate_pnl(binary_factors, rocp_dict):
+    pnl_dict = {}
+    """
+    Main function that calculates the PnL for each commodity
+    """
+    # Calculate the profit and loss for each commodity
+    for commodity in binary_factors.columns:
+        binary_factor = binary_factors[commodity].shift(-1)  # Shift the binary factor by one day
+        rocp1_values = rocp_dict.get(commodity)  # Use get() method to prevent KeyError
+        if rocp1_values is not None:
+            pnl = binary_factor * rocp1_values * 1000000
+            pnl_dict[commodity] = pnl
+        
+    pnl_df = pd.DataFrame(pnl_dict)
+    pnl_df.dropna(inplace=True)  # Drop rows containing NaN values
+
+    return pnl_df
+
+def expand_factor(factor, rocp1_dict):
+    """
+    for factor 1A and 2A, want to repicate signals across all commodities
+    """
+    expanded_factor = pd.DataFrame()
+    for commodity in rocp1_dict.keys():
+        expanded_factor[commodity] = factor[factor.columns[0]]
+    expanded_factor.index = factor.index  # Ensure the expanded factor has the same index as the original factor
+    return expanded_factor
+
+
+def calculate_pnl_expanded_factor(factor, rocp1_dataframe):
+
+    expanded_factor = expand_factor(factor, rocp1_dataframe)
+    return calculate_pnl(expanded_factor, rocp1_dataframe)
+
+
+
+factor1A_pnl = calculate_pnl_expanded_factor(factor1A(), rocp1_dataframe)
+factor1B_pnl = calculate_pnl(factor1B(), rocp1_dataframe)
+factor2A_pnl = calculate_pnl_expanded_factor(factor2A(), rocp1_dataframe)
+factor2B_pnl = calculate_pnl(factor2B(), rocp1_dataframe)
+factorA_0304_pnl = calculate_pnl(factorA_0304(), rocp1_dataframe)
+factorB_0304_pnl = calculate_pnl(factorB_0304(), rocp1_dataframe)
+
+
+def portfolio_pnl(factor_names, *pnl_dataframes):
+    individual_pnls = {}
+    
+    for i, pnl_df in enumerate(pnl_dataframes):
+        daily_pnl = pnl_df.sum(axis=1)
+        cumulative_pnl = daily_pnl.cumsum()
+
+        individual_pnls[factor_names[i]] = pd.DataFrame({
+            'Daily PnL': daily_pnl,
+            'Cumulative PnL': cumulative_pnl
+        })
+
+    return individual_pnls
+
+factor_names = ["Factor 1A", "Factor 1B", "Factor 2A", "Factor 2B", "Factor A_0304", "Factor B_0304"]
+individual_pnls = portfolio_pnl(factor_names, factor1A_pnl, factor1B_pnl, factor2A_pnl, factor2B_pnl, factorA_0304_pnl, factorB_0304_pnl)
+
+
+factor1A_pnl_df = individual_pnls['Factor 1A']
+factor1B_pnl_df = individual_pnls['Factor 1B']
+factor2A_pnl_df = individual_pnls['Factor 2A']
+factor2B_pnl_df = individual_pnls['Factor 2B']
+factor34A_pnl_df = individual_pnls['Factor A_0304']
+factor34B_pnl_df = individual_pnls['Factor B_0304']
+rvpnl=[factor1A_pnl_df, factor1B_pnl_df, factor2A_pnl_df, factor2B_pnl_df, factor34A_pnl_df, factor34B_pnl_df]
+
+def pnl_to_xl(truncate = None):
+    names = ['Factor 1A','Factor 1B','Factor 2A','Factor 2B','Factor34A','Factor34B']
+    writer=pd.ExcelWriter('pnl.xlsx')
+    for i, A in enumerate(rvpnl):
+        A.to_excel(writer,sheet_name="{0}".format(names[i]))
+    writer.save()
